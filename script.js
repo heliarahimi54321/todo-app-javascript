c41e1ab22e41305b0d76c246a7242d250b90cf95
Helia Rahimi <helia.rahimi54321@gmail.com>
2026-04-15 13:47:16 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+function addTask() {
+    let input = document.getElementById("taskInput");
+    let taskText = input.value;
+
+    if (taskText === "") return;
+
+    let li = document.createElement("li");
+    li.textContent = taskText;
+
+    li.onclick = function() {
+        li.remove();
+    };
+
+    document.getElementById("taskList").appendChild(li);
+
+    input.value = "";
+}
